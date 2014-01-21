0e63643088a16d4b32f73a2a3602fe7d31da21b4
Jacek Złydach <temporal.pl@gmail.com>
2014-01-21 20:43:37 +0100

    Skeleton.

--- main.lisp
+++ main.lisp
@@ -8,6 +8,15 @@
            :mute
            :unmute))
 
+(in-package :alice)
+
+(defvar *connection*)
+(defvar *nick* "")
+(defvar *connected-channels*)
+
+(defconstant +nickserv+ "NickServ")
+(defconstant +nickserv-identify-msg-template+ "IDENTIFY %a")
+
 (defun run-test (nick pass)
   (let ((connection (irc:connect :nickname nick
                                  :server "irc.freenode.net")))
@@ -15,9 +24,23 @@
     (irc:join connection "#trc")
     (irc:privmsg connection "#trc" "Ta Da!")))
 
+(defun msg-hook (message)
+  (format t "~a~%" message))
 
 (defun start-alice (server nick pass &rest channels)
-  (setf *))
+  (setf *nick* nick)
+  (setf *connection* (connect :nickname *nick*
+                              :server server))
+  (setf *connected-channels* channels)
+
+  (privmsg *connection* +nickserv+ (format nil +nickserv-identify-msg-template+ pass))
+
+  (mapcar (lambda (channel) (join *connection* channel)) channels)
+
+  (add-hook *connection* 'irc::irc-privmsg-message 'msg-hook)
+  #+(or sbcl
+        openmcl)
+  (start-background-message-handler *connection*))
 
 (defun stop-alice ()
   (format t "Not yet implemented.~%"))
