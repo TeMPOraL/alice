2f86202fb7ac3bb35d63fa69b5bdf659373de6dc
Jacek Złydach <temporal.pl@gmail.com>
2014-01-26 15:20:38 +0100

    Local config template.

---
+++ local-config-template.lisp
@@ -0,0 +1,9 @@
+(in-package :alice)
+
+(setf *pushover-token* "insert token"
+      *pushover-user* "insert user"
+
+      *server* "irc.freenode.net"
+      *nick* "Alice_M"
+      *password* "insert password"
+      *autojoin-channels* '("#hackerspace-krk" "#TRC"))
