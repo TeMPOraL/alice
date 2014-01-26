3843c8907351afe873d11808a0539400ddb40372
Jacek Złydach <temporal.pl@gmail.com>
2014-01-26 19:55:27 +0100

    Some TODOs + comments for interested.

--- main.lisp
+++ main.lisp
@@ -37,7 +37,14 @@
 (defconstant +nickserv+ "NickServ")
 (defconstant +nickserv-identify-msg-template+ "IDENTIFY ~a")
 
-;; phrases
+;;; phrases
+;; phrases are defined as an alist of label and possible phrases to speak
+;; plain-text value will be said directly
+;; list of values -> bot will pick one at random
+;; vector of values -> bot will output all of them sequencially, possibly throttling output to avoid flooding the channel
+;; 
+;; values are read recursively, i.e. encoding a symbol will cause bot to find the proper phrase in
+;; this alist, and e.g. list of vector means "pick a sequence of things to say at random"
 (defparameter *answers* 
   '((:introduction . ("Alice Margatroid, do usług."
                        "Alice Margatroid, kłaniam się ;)."
@@ -311,6 +318,7 @@
         ((and is-directed
               (or (mentions "tak" message-body)
                   (mentions "yes" message-body)
+                  (mentions "dawaj" message-body)
                   (mentions "pros" message-body))
               (not (null *throttled-output*)))
          (say destination *throttled-output*))
