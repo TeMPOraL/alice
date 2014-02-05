492c81e630e5f5429853c180f8fc196b8ee8d75b
Jacek Złydach <temporal.pl@gmail.com>
2014-02-05 01:44:55 +0100

    version number update

--- globals.lisp
+++ globals.lisp
@@ -38,7 +38,7 @@
                       "Alice Margatroid, the Seven-Colored Puppeteer."
                       "Pozornie Zapracowana Youkai, Alice Margatroid."))
 
-    (:version . "0.0.34a. (The Girl Who Shortened Some Stuff)")
+    (:version . "0.0.35. (The Girl Who Didn't Give a Single Damn About the Case)")
 
     (:smiles . (":)" ":)" ":)" ":)" ":)" ":)" ":)" ":)" ":)" ":)" ; yeah, a cheap trick to fake probability distribution
                 ";)" ";)" ";)"";)" ";)" ";)"
