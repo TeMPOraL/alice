ab6e85ca83a7f2fac306600d680713cc26615ebc
Jacek Złydach <temporal.pl@gmail.com>
2014-04-17 02:16:27 +0200

    Sorted out days of week as well.

--- language.lisp
+++ language.lisp
@@ -58,31 +58,15 @@
     (:tomorrow . ("jutro"))
     (:day-after-tomorrow . ("pojutrze"))
     ;; 
-    (:monday . "w poniedziałek")
-    (:tuesday . "we wtorek")
-    (:wednesday . "w środę")
-    (:thursday . "w czwartek")
-    (:friday . "w piątek")
-    (:saturday . "w sobotę")
-    (:sunday . "w niedzielę")
-    ;; 
-    (:prev-monday . ("w zeszły poniedziałek"
-                     "w ostatni poniedziałek"))
-    (:prev-tuesday . ("w zeszły wtorek"
-                      "w ostatni wtorek"))
-    (:prev-wednesday . ("w zeszłą środę"
-                     "w ostatnią środę"))
-    (:prev-thursday . ("w zeszły czwartek"
-                      "w ostatni czwartek"))
-    (:prev-friday . ("w zeszły piątek"
-                     "w ostatni piątek"))
-    (:prev-saturday . ("w zeszłą sobotę"
-                      "w ostatnią sobotę"))
-    (:prev-sunday . ("w zeszłą niedzielę"
-                     "w ostatnią niedzielę"))))
+    (:monday . ("w poniedziałek"))
+    (:tuesday . ("we wtorek"))
+    (:wednesday . ("w środę"))
+    (:thursday . ("w czwartek"))
+    (:friday . ("w piątek"))
+    (:saturday . ("w sobotę"))
+    (:sunday . ("w niedzielę"))))
 
-(defparameter *days-of-week* #(:sunday :monday :tuesday :wednesday :thursday :friday :saturday))
-(defparameter *days-of-previous-week* #(:prev-sunday :prev-monday :prev-tuesday :prev-wednesday :prev-thursday :prev-friday :prev-saturday))
+(defparameter *days-of-week* #(:monday :tuesday :wednesday :thursday :friday :saturday :sunday))
 
 (defparameter *days-offset-alist* '((0 . :today)
 
@@ -99,30 +83,35 @@
 (defvar +timestring-time-format+ '((:HOUR 2) #\: (:MIN 2) #\: (:SEC 2)))
 
 ;; DAYS
-
 (defun days-diff (time-a time-b)        ;FIXME should go to some sort of date utils
   "Compute the difference in calendar days between `TIME-A' and `TIME-B'."
   (- (local-time:day-of (local-time:timestamp-minimize-part time-a :hour))
      (local-time:day-of (local-time:timestamp-minimize-part time-b :hour))))
+
+(defun fix-day-of-week-offset (offset)
+  (mod (1- offset) 7))
+
+(defun timestamp-day-of-week (timestamp)
+  (local-time:with-decoded-timestamp (:day-of-week d-o-w) timestamp d-o-w))
+
+(defun get-date-day-of-week-phrase (timestamp reference)
+  (let ((days-offset (days-diff reference timestamp))
+        (day-of-week (fix-day-of-week-offset (timestamp-day-of-week timestamp))))
+    (when (and (> days-offset 0)
+             (< days-offset 7))
+        (elt *days-of-week* day-of-week))))
 
 (defun get-relative-date-difference-phrase (time-a time-b)
   "Compute natural-language date difference of days between timestamps `TIME-A' and `TIME-B'. Return a proper symbol or NIL if no natural-language representation is available."
   (let ((days-offset (days-diff time-a time-b)))
     (cdr (assoc days-offset *days-offset-alist*))))
 
+
 (defun format-date (timestamp &optional (reference (local-time:now)))
-  (let ((phrase (get-relative-date-difference-phrase reference timestamp)))
+  (let ((phrase (or (get-relative-date-difference-phrase reference timestamp)
+                    (get-date-day-of-week-phrase timestamp reference))))
     (if phrase (alexandria:random-elt (cdr (assoc phrase *date-difference-strings*))) ;FIXME use say-equivalent or sth
         (local-time:format-timestring nil timestamp :format +timestring-date-format+))))
-
-
-(defun fix-day-of-week-offset (offset)
-  (mod (1- offset) 7))
-
-(defun day-of-week-offset-string (time-a time-b)
-  (let ((days-offset (days-diff time-a time-b)))
-    
-  ))
 
 ;; TIME
 (defun format-time (timestamp)
