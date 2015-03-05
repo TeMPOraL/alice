8e01786d0d423b520b0060edfe6e960147b78b74
TeMPOraL <temporal.pl@gmail.com>
2015-03-05 04:06:41 +0100

    Some notes on event loop.

--- core/trivial-event-loop.lisp
+++ core/trivial-event-loop.lisp
@@ -1,1 +1,7 @@
 (in-package #:alice)
+
+;;; https://trac.common-lisp.net/bordeaux-threads/wiki/ApiDocumentation
+;;; #'condition-wait and #'condition-notify could be used to sync the threads on event loop.
+
+;;; also, consider moving timer for delayed actions into a separate
+;;; unit with separate thread, that feeds events back to the primary event loop.
