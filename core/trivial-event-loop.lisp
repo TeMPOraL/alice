8dbbb966b8ed4a6dddbfac5028247f5c5f30e54e
TeMPOraL <temporal.pl@gmail.com>
2015-03-07 01:02:09 +0100

    Implemented event queue.

--- core/trivial-event-loop.lisp
+++ core/trivial-event-loop.lisp
@@ -1,7 +1,53 @@
 (in-package #:alice)
 
+(defvar *event-queue-lock* (bordeaux-threads:make-lock))
+(defvar *event-queue* '())
+
+(defvar *event-loop-timer* (trivial-timers:make-timer (lambda () (funcall 'process-event-queue))))
+
+(defun process-event-queue ()
+  "Process event in global event queue."
+  (let ((to-execute '()))
+    (bordeaux-threads:with-lock-held (*event-queue-lock*)
+      (setf *event-queue* (remove-if #'null (mapcar (lambda (event)
+                                                      (when (funcall (car event))
+                                                        (push (cdr event) to-execute)
+                                                        event))
+                                                    *event-queue*))))
+    (mapc #'funcall to-execute)))
+
+(defun add-event (condition action)
+  "Register an event made out of `CONDITION' and `ACTION' for single execution."
+  (bordeaux-threads:with-lock-held (*event-queue-lock*)
+    (push (cons condition action)  *event-queue*)))
+
+
+(defun run-event-loop ()
+  (unless (trivial-timers:timer-scheduled-p *event-loop-timer*)
+    (trivial-timers:schedule-timer *event-loop-timer* 0 :repeat-interval 1)))
+
+(defun stop-event-loop ()
+  (when (trivial-timers:timer-scheduled-p *event-loop-timer*)
+    (trivial-timers:unschedule-timer *event-loop-timer*)))
+
 ;;; https://trac.common-lisp.net/bordeaux-threads/wiki/ApiDocumentation
 ;;; #'condition-wait and #'condition-notify could be used to sync the threads on event loop.
 
 ;;; also, consider moving timer for delayed actions into a separate
 ;;; unit with separate thread, that feeds events back to the primary event loop.
+
+
+;;; utility event condition functions
+
+(defun run-asap ()
+  (lambda () t))
+
+(defun run-after-delay-msec (delay)
+  (lambda () nil
+     ;; TODO
+     ))
+
+(defun run-after-time (time)
+  (lambda () nil
+     ;; TODO
+     ))
