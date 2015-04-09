3dfa0733c220186cab4e22c9739ad2bd8fe6b204
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2015-04-09 08:17:40 +0200

    Macroifed tests further; stubbed tests for all input matchers.

--- tests/test-suites/input-matchers.lisp
+++ tests/test-suites/input-matchers.lisp
@@ -34,10 +34,12 @@
                                "#test")))
 
 (defun best-match (match input)
+  "Test if `MATCH' is the best match for `INPUT'."
   (eql match
        (car (first (alice::best-matchers input)))))
 
 (defun only-primary-match (match input)
+  "Test if `MATCH' is the best match for `INPUT' and that there are no other matches that tied."
   (flet ((remove-non-equally-scored-matches (score matches)
            (remove-if-not (lambda (match)
                             (= (cdr match) score))
@@ -48,20 +50,111 @@
           (eql match
                (car (first best-matches)))))))
 
-;;; tests
-(test version-number
-  (with-temporary-environment
-    (mapc (lambda (input)
-            (is (only-primary-match :version (apply 'make-input input))))
-          `(
-           ;; pl_PL
-            ("Alice_M, wersja")
-            ("Alice_M, jaką masz wersję?")
-            ("Jaki masz numer wersji, Alice_M?")
-            ("Alice_M: podaj numer wersji!")
+(defmacro test-is-primary-match (name matchspec &body inputs)
+  "Create test named `NAME' which ensures that each of `INPUTS' strings generates an distinct match to `MATCHSPEC'."
+  `(test ,name
+     (with-temporary-environment
+       (mapc (lambda (input)
+               (is (only-primary-match ,matchspec (apply 'make-input input))))
+             ',inputs))))
 
-            ;; en_US
-            ("Alice_M, version")
-            ("Alice_M, version number please!")))))
+;;; tests
+
+(test-is-primary-match primary-match-version :version
+  ;; pl_PL
+  ("Alice_M, wersja")
+  ("Alice_M, jaką masz wersję?")
+  ("Jaki masz numer wersji, Alice_M?")
+  ("Alice_M: podaj numer wersji!")
+
+  ;; en_US
+  ("Alice_M, version")
+  ("Alice_M, version number please!"))
+
+(test-is-primary-match primary-match-wolfram-alpha-query-full :wolfram-alpha-query-full
+  ("Foo"))
+
+(test-is-primary-match primary-match-wolfram-alpha-query :wolfram-alpha-query
+  ("Foo"))
+
+(test-is-primary-match primary-match-whois :whois
+  ("Foo"))
+
+(test-is-primary-match primary-match-assign-name-alias :assign-name-alias
+  ("Foo"))
+
+(test-is-primary-match primary-match-shorten-url :shorten-url
+  ("Foo"))
+
+(test-is-primary-match primary-match-notify-user :notify-user
+  ("Foo"))
+
+(test-is-primary-match primary-match-delayed-notify-user :delayed-notify-user
+  ("Foo"))
+
+(test-is-primary-match primary-match-track-package :track-package
+  ("Foo"))
+
+(test-is-primary-match primary-match-github-issues-link :github-issues-link
+  ("Foo"))
+
+(test-is-primary-match primary-match-add-github-issue :add-github-issue
+  ("Foo"))
+
+(test-is-primary-match primary-match-random-frequency :random-frequency
+  ("Foo"))
+
+(test-is-primary-match primary-match-whats-the-frequency :whats-the-frequency
+  ("Foo"))
+
+(test-is-primary-match primary-match-events-list :events-list
+  ("Foo"))
+
+(test-is-primary-match primary-match-marisa :marisa
+  ("Foo"))
+
+(test-is-primary-match primary-match-introductions :introductions
+  ("Foo"))
+
+(test-is-primary-match primary-match-thanks-reply :thanks-reply
+  ("Foo"))
+
+(test-is-primary-match primary-match-sing :sing
+  ("Foo"))
+
+(test-is-primary-match primary-match-talking-about-me :talking-about-me
+  ("Foo"))
+
+(test-is-primary-match primary-match-tcp-handshake :tcp-handshake
+  ("Foo"))
+
+(test-is-primary-match primary-match-hello :hello
+  ("Foo"))
+
+(test-is-primary-match primary-match-repo-link :repo-link
+  ("Foo"))
+
+(test-is-primary-match primary-match-dice-throw :dice-throw
+  ("Foo"))
+
+(test-is-primary-match primary-match-goodnight :goodnight
+  ("Foo"))
+
+(test-is-primary-match primary-match-makes-sense-troll :makes-sense-troll
+  ("Foo"))
+
+(test-is-primary-match primary-match-coincidence? :coincidence?
+  ("Foo"))
+
+(test-is-primary-match primary-match-yolo :yolo
+  ("Foo"))
+
+(test-is-primary-match primary-match-throttle-continue :throttle-continue
+  ("Foo"))
+
+(test-is-primary-match primary-match-default-response :default-response
+  ("Foo"))
+
+
 
 (add-suite 'input-matchers)
