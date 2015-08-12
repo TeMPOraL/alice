f4b122ef5a1da58bfdf2baef5e5de9b36705c6e1
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2015-08-13 01:09:10 +0200

    Version number update.

--- version.lisp
+++ version.lisp
@@ -1,4 +1,4 @@
 (in-package :alice)
 
 ;;; NOTE: do not alter the structure of this file and the following expression in particular - it's also used by ASDF to determine version number.
-(defparameter *version* (concatenate 'string "0.2.1" " (ta uczona przez Marsjan)"))
+(defparameter *version* (concatenate 'string "0.2.2" " (ta parsująca pauzy w nickach)"))
