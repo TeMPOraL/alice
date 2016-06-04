f7f5877e23a29c55425607e887a73ec4c8117a13
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2016-06-04 21:19:12 +0200

    Version number bump.

--- version.lisp
+++ version.lisp
@@ -1,4 +1,4 @@
 (in-package :alice)
 
 ;;; NOTE: do not alter the structure of this file and the following expression in particular - it's also used by ASDF to determine version number.
-(defparameter *version* (concatenate 'string "0.2.3" " (ta z elementarnym persistence)"))
+(defparameter *version* (concatenate 'string "0.3.1" " (ta z elementarną zdalną administracją)"))
